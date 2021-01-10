922446d896f4764c52e168923f6f9aa0d3d6054d
kzri4 <kazu4126ab@gmail.com>
2021-01-11 00:13:55 +0900

    fifth commit

--- index.php
+++ index.php
@@ -6,13 +6,17 @@ require_once('functions.php');
 $dbh = connectDb();
 
 $sql = 'SELECT
-        sales.year,sales.month,branches.name,staffs.name,sales.sale
+        sales.year,
+        sales.month,
+        branches.name as branch_name,
+        staffs.name as staff_name,
+        sales.sale
         FROM 
         sales
         inner JOIN staffs
         ON sales.staff_id = staffs.id
         inner JOIN branches
-        ON staffs.branch_id = branches.id' ;
+        ON staffs.branch_id = branches.id';
 
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
@@ -21,26 +25,27 @@ $sales = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $sum = 0;
 foreach ($sales as $sale){
 $sum += $sale['sale'];
-
 }
 
-/*
-if (isset($_GET['year'])){
-    $sql = 'SELECT year FROM sales';
+if (isset($_GET['year'])&&($_GET['branch'])&&($_GET['staff'])){
+    $sql = 'SELECT
+        sales.year,
+        sales.month,
+        branches.name as branch_name,
+        staffs.name as staff_name,
+        sales.sale
+        FROM 
+        sales
+        inner JOIN staffs
+        ON sales.staff_id = staffs.id
+        inner JOIN branches
+        ON staffs.branch_id = branches.id 
+        WHERE year = 1 AND branch_name = 2 AND staff_name =3';
+    
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
-    $sales = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-if (isset($_GET['staff'])){
-    $sql = 'SELECT staffs.name,branches.name
-            FROM staffs
-            INNER JOIN branches
-            ON staffs.branch_id = branches.id;' ;
-    $stmt = $dbh->prepare($sql);
-    $staffs = $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-*/
 
 ?>
 
@@ -61,32 +66,40 @@ if (isset($_GET['staff'])){
         <div class="year">
             <lablel>年</lablel> 
             <select name="year"> 
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-                <option value="2020"selected>2020</option>
-                <option value="2021">2021</option>
+                <?foreach ($sales as $sale):?>
+                <option value = 1 
+                <?php echo array_key_exists('year', $_GET) 
+                && $_GET['year'] == '1'?'selected':'';?>>
+                <?= h($sale['year']) ?>
+                </option>
+                <?endforeach;?>
             </select>
         </div>
 
         <div class="branch">
             <lablel>支店</lablel>
             <select name="branch"> 
-                <option value="岩手">岩手</option>
-                <option value="東京" selected>東京</option>
-                <option value="福岡">福岡</option>
-                <option value="徳島">徳島</option>
-                <option value="北海道">北海道</option>
+                <?foreach ($sales as $sale):?>
+                <option value = 2
+                <?php echo array_key_exists('branch', $_GET) 
+                && $_GET['branch'] == '2'?'selected':'';?>>
+                <?= h($sale['branch_name']) ?>
+                </option>
+                <?endforeach;?>
             </select>
         </div>
 
         <div class="staff">
             <lablel>従業員</lablel>
             <select name="staff"> 
-                <option value="田中">田中</option>
-                <option value="山田" selected>山田</option>
-                <option value="渡辺">渡辺</option>
-                <option value="松田">松田</option>
-                <option value="野中">野中</option>
+                <?foreach ($sales as $sale):?>
+                <option value = 3
+                <?php echo array_key_exists('staff', $_GET) 
+                && $_GET['staff'] == '3'?'selected':'';?>>
+                <?= h($sale['staff_name']) ?>
+            </option>
+                <?endforeach;?>
+
         </select><br>
         </div>
     </div>
@@ -104,13 +117,16 @@ if (isset($_GET['staff'])){
         <th>従業員</th>
         <th>売上</th>
     </tr>
+    <?foreach ($sales as $sale):?>
     <tr>
         <td width="300"><?= h($sale['year']) ?></td>
         <td width="300"><?= h($sale['month']) ?></td>
-        <td width="300"><?= h($sale['name']) ?></td>
-        <td width="300"><?= h($sale['name']) ?></td>
+        <td width="300"><?= h($sale['branch_name']) ?></td>
+        <td width="300"><?= h($sale['staff_name']) ?></td>
         <td width="300"><?= h($sale['sale']) ?></td>
     </tr>
+    <?endforeach;?>
+    
 </table>
 
 <h1 class="cal">合計:<?= $sum ?></h1>
